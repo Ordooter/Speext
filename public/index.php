f73e5bbc3b38868f55245c70fb2b243c7468c4bd
Butu Ordooter <ordooter@gmail.com>
2018-10-21 23:46:40 +0100

    Updated Microphone Icon.

--- public/index.php
+++ public/index.php
@@ -6,13 +6,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Speech Recognition</title>
+  <title>Speext</title>
     <link rel="stylesheet" href="css/app.css">
   <link rel="stylesheet" href="css/style.css">
   <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 </head>
 <body>
+    <h3> A Simple Speech To Text Recognition App, Using Laravel Framework and Powered by Javascript</h3>
   <div class="container"> <!--page container -->
     <div class="text-box" contenteditable="true"></div> <!--text box which will contain spoken text -->
       <i class="fa fa-microphone"></i> <!-- microphone icon to be clicked before speaking -->
